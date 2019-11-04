7b6ba70e8ed48a3ac1a820b9d8b0bd26d44080aa
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2019-11-04 03:13:44 -0300

    Mudanças de style do projeto

--- DesAperto_1/src/index.js
+++ DesAperto_1/src/index.js
@@ -3,6 +3,14 @@ import Routes from "./routes";
 import "./config/statusBarConfig";
 
 
-const App = () => <Routes />;
+const App = () => <Routes/>;
+
+/*const app = require("./config/express")();
+
+require("./config/database");
+
+app.listen(app.get("port"), () => {
+	console.log("Servidor rodando na porta 3001...");
+})*/
 
 export default App;
