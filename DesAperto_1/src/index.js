8362ea192b8746be2a1e51cee0ec3779dfe34b39
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2019-11-04 19:05:33 -0300

    Ajuste de codigo

--- DesAperto_1/src/index.js
+++ DesAperto_1/src/index.js
@@ -2,15 +2,6 @@ import React from "react";
 import Routes from "./routes";
 import "./config/statusBarConfig";
 
-
-const App = () => <Routes/>;
-
-/*const app = require("./config/express")();
-
-require("./config/database");
-
-app.listen(app.get("port"), () => {
-	console.log("Servidor rodando na porta 3001...");
-})*/
+const App = () => <Routes />;
 
 export default App;
