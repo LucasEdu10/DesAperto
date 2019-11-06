0826f7b37dc762592970dff5d4d994cc9b6bd933
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2019-11-06 02:34:36 -0300

    Adição de pacotes para usar e ler o QRcode

--- DesAperto_1/android/app/src/main/java/com/desaperto_1/MainApplication.java
+++ DesAperto_1/android/app/src/main/java/com/desaperto_1/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
+import org.reactnative.camera.RNCameraPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
