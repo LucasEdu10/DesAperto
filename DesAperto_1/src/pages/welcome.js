79757a09c30ff2f420bfaa4edb0445081cac5556
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2019-11-06 02:31:49 -0300

    Adicionando imagens para o menu

---
+++ DesAperto_1/src/pages/welcome.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import { View,
+		Text,
+		ScrollView,
+		FlatList,
+		StyleSheet,
+		Button,
+		Alert
+		} from "react-native";
+
+export default class Welcome extends Component {
+
+	render() {
+		return(
+			/*<View>
+				<Text style={styles.list}> Ultimas transações </Text>
+			</View>*/
+			<ImageBackground source={...} style={{width: '100%', height: '100%'}}>
+    		<Text>Inside</Text>
+ 			</ImageBackground>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+
+});
