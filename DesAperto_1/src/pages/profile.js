c365acf9a14776e891c1985c3fff4ff1c9e71de3
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2019-11-17 18:24:34 -0300

    Ajuste de codigo

--- DesAperto_1/src/pages/profile.js
+++ DesAperto_1/src/pages/profile.js
@@ -11,7 +11,7 @@ export default class Profile extends Component {
 	render(){
 		return(
 			<View style={styles.container}>
-				<Text style={styles.text}> Olá {this.state.displayName}</Text>
+				<Text style={styles.text}> Olá </Text>
 			</View>
 			);
 	}
